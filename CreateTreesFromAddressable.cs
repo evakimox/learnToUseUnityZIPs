f1f8adfb04ec17e0c7e9bfaa16339f1291a1417d
Jingning <zhangjn1121@gmail.com>
2020-02-14 18:19:07 +0800

    附在场景中的随便某个对象上，就可以从Addressable Asset中找到并生成一个gameObject的脚本

---
+++ CreateTreesFromAddressable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class CreateTreesFromAddressable : MonoBehaviour
+{
+    bool treesGenerated;
+    void Start()
+    {
+        treesGenerated = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (!treesGenerated)
+        {
+            // 通过Addressable Asset生成松树
+            // 这个参数自己改，要生成什么，就把参数改成Addressable勾选框后面的那个字符串
+            Addressables.InstantiateAsync("fiveTreesPrefab").Completed += OnLoadTreeDone;
+            treesGenerated = true;
+        }
+    }
+
+    void OnLoadTreeDone(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<GameObject> obj)
+    {
+        try
+        {
+            GameObject createdObj = obj.Result;
+            createdObj.transform.position = transform.position;
+            createdObj.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        catch(UnityException e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
